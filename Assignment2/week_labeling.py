9c1d7f539d2bcae09d8b58cbb6dfff52ab2c6a57
james.sullivan <jesullivan222@gmail.com>
2019-07-24 13:24:42 -0400

    Completed week_labeling.py, including comments & exceptions.

--- Assignment2/week_labeling.py
+++ Assignment2/week_labeling.py
@@ -16,8 +16,6 @@ Rule:    If the annualized return of a given 10 day block is greater than or
          of the S&P 500 since its inception, NOT adjusted for inflation.
 
          Annualized Return = ((1 + cumulative return) ^ (365/days held)) - 1
-                                    OR
-         Annualized Return = ((1 + cumulative return) ^ (365/5)) - 1
 
 Strategy:
     1. For 1st "green" week, invest $100 by buys shares of stock at opening
@@ -47,131 +45,182 @@ import pandas as pd
 import numpy as np
 import os
 
-ticker='SBUX'
-#input_dir = r'C:\Users\james\BU MET\CS677\Datasets'
-input_dir = r'C:\Users\james.sullivan\Documents\Personal Documents - Temporary (Delete)\BU MET\CS677\Datasets'
-ticker_file = os.path.join(input_dir, ticker + '.csv')
+try:
+    ticker='SBUX'
+    #input_dir = r'C:\Users\james\BU MET\CS677\Datasets'
+    input_dir = r'C:\Users\james.sullivan\Documents\Personal Documents - Temporary (Delete)\BU MET\CS677\Datasets'
+    ticker_file = os.path.join(input_dir, ticker + '.csv')
+    df = pd.read_csv(ticker_file)
 
-df = pd.read_csv(ticker_file)
+except Exception as e:
+    print(e)
+    print("Could not read stock dataset")
 
-pd.set_option('display.max_columns', 7)
+try:
+    # create new df with cumulative returns for each week
+    week_return_df = df[['Year_Week', 'Return']].groupby(
+        'Year_Week').sum().reset_index()
 
-#df['Return'] = 100.0 * df['Return']     # Percent return
+    # rename 'Return' to 'Return_Week' and merge back into original df
+    week_return_df.rename(columns={'Return': 'Return_Week'}, inplace=True)
+    df = pd.merge(df, week_return_df, on='Year_Week')
 
-y1_start_date = '2014-01-02'
-y1_end_date = '2014-12-31'
+    # create new df with the length of each week (by days)
+    week_size_df = df[['Year_Week', 'Return']].groupby(
+        'Year_Week').count().reset_index()
 
-y2_start_date = '2015-01-02'
-y2_end_date = '2015-12-31'
+    # rename 'Return' to 'Week_Size' and merge back into original df
+    week_size_df.rename(columns={'Return': 'Week_Size'}, inplace=True)
+    df = pd.merge(df, week_size_df, on='Year_Week')
 
-size_2014 = len(df[df['Year'] == 2014])
-size_2015 = len(df[df['Year'] == 2015])
+    # create new df w/ the date of the first day of each week
+    week_start_df = df[['Year_Week', 'Date']].groupby(
+        'Year_Week').min().reset_index()
+    week_start_df.rename(columns={'Date': 'Week_Start'}, inplace=True)
 
-year_list = df['Year'].unique()
+    # create new df w/ the date of the last day of each week
+    week_end_df = df[['Year_Week', 'Date']].groupby(
+        'Year_Week').max().reset_index()
+    week_end_df.rename(columns={'Date': 'Week_End'}, inplace=True)
 
+    # merge week_start_df and week_end_df, then merge new df into original df
+    week_bounds_df = pd.merge(week_start_df, week_end_df, on='Year_Week')
+    df = pd.merge(df, week_bounds_df, on='Year_Week')
 
-
-year_week_df = df[['Year_Week', 'Return']].groupby('Year_Week').sum().reset_index()
-year_week_df.rename(columns={'Return': 'Return_Week'}, inplace=True)
-df = pd.merge(df, year_week_df, on='Year_Week')
-
-week_size_df = df[['Year_Week', 'Return']].groupby('Year_Week').count().reset_index()
-week_size_df.rename(columns={'Return': 'Week_Size'}, inplace=True)
-df = pd.merge(df, week_size_df, on='Year_Week')
-
-
-df_test = df[['Year_Week', 'Date']].groupby('Year_Week').min().reset_index()
-df_test.rename(columns={'Date': 'Week_Start'}, inplace=True)
-
-df_test_2 = df[['Year_Week', 'Date']].groupby('Year_Week').max().reset_index()
-df_test_2.rename(columns={'Date': 'Week_End'}, inplace=True)
-
-week_bounds_df = pd.merge(df_test, df_test_2, on='Year_Week')
-df = pd.merge(df, week_bounds_df, on='Year_Week')
+except KeyError as e:
+    print(e)
+    print("Key value not found")
 
 
 def annualized_return(vec):
-    r = vec[0]
-    length = vec[1]
-    return (1 + r) ** (365 / length) - 1
+    """
+    Returns the annualized return value of a given cumulative return over
+    a specified length of time. Must be performed on array where 1st col
+    is the cumulative return, and the 2nd col is the day length associated
+    with the cumulative return value.
+    :param vec: Vector from DataFrame
+    :return: Annualized Return value
+    """
+
+    try:
+        r = vec[0]
+        length = vec[1]
+        return (1 + r) ** (365 / length) - 1
+
+    except KeyError as ke:
+        print(ke)
+        print('annualized_return() could not find keys')
 
 
-df['Annual_Return'] = df[['Return_Week', 'Week_Size']].apply(annualized_return, axis=1)
-df['Annual_Return_%'] = df['Annual_Return'].apply(lambda x: x * 100)
+try:
+    # calculate annualized return for each week
+    df['Annual_Return'] = df[['Return_Week', 'Week_Size']].apply(
+        annualized_return, axis=1)
 
+    # calculate percent annualized return for each week
+    df['Annual_Return_%'] = df['Annual_Return'].apply(lambda x: x * 100)
 
-df['Color'] = df['Annual_Return_%'].apply(lambda x: 'Green' if x >= 10 else 'Red')
+    # assign color based on percent annualized return value
+    df['Color'] = df['Annual_Return_%'].apply(
+        lambda x: 'Green' if x >= 10 else 'Red')
+
+except ValueError as color_exception:
+    print(color_exception)
 
 
 def buy_and_hold(data):
-
+    """
+    Buy stock on 1st day and sell stock on the last day
+    :param data: DataFrame of stock data
+    :return: Returns the final funds after following strategy
+    """
     initial_funds = 100.00  # funds start at $100.00
     initial_price = data.iloc[0]['Open']
     final_price = data.iloc[len(data) - 1]['Adj Close']
-    final_funds = final_price * (initial_funds / initial_price)  # price * shares
+    final_funds = final_price * (initial_funds / initial_price) # price * shares
     return final_funds
 
 
 def color_strategy(data):
+    """
+    Follows strategy described in the docstring at the top of this file
+    :param data: DataFrame of stock data
+    :return: Returns the final funds after following strategy
+    """
 
     funds = 100.00  # funds start at $100.00
-    shares = 0.0
+    shares = 0.0    # shares start at $0.00
     current_color = 'Red'   # initialize color to red
 
-    week_data = data[['Year_Week', 'Color', 'Week_Start', 'Open']].groupby('Year_Week').first().reset_index()
+    try:
+        # get week start date and open value on that date
+        week_data = data[['Year_Week', 'Color', 'Week_Start', 'Open']].groupby(
+            'Year_Week').first().reset_index()
 
-    week_end_data = data[['Year_Week', 'Week_End', 'Adj Close']].groupby('Year_Week').last().reset_index()
+        # get week end date and adjusted close value on that date
+        week_end_data = data[['Year_Week', 'Week_End', 'Adj Close']].groupby(
+            'Year_Week').last().reset_index()
 
-    week_data = pd.merge(week_data, week_end_data, on='Year_Week')
+        # merge DataFrames w/ week_start and week_end info
+        week_data = pd.merge(week_data, week_end_data, on='Year_Week')
 
-    if week_data.iloc[0]['Color'] == 'Green':
-        shares = funds / data[0]['Open']
-        funds = 0.00
-        current_color = 'Green'
+        # purchase funds on first day of first week, if 1st week color is green
+        if week_data.iloc[0]['Color'] == 'Green':
+            shares = funds / data[0]['Open']
+            funds = 0.00
+            current_color = 'Green'
 
-    for i in range(len(week_data)-1):
+        # iterate through weeks
+        for i in range(len(week_data)-1):
 
-        next_color = week_data.iloc[i+1]['Color']
-        open_price = week_data.iloc[i]['Open']
-        adj_close_price = week_data.iloc[i]['Adj Close']
+            next_color = week_data.iloc[i+1]['Color']      # next week's color
+            open_price = week_data.iloc[i]['Open']              # open price
+            adj_close_price = week_data.iloc[i]['Adj Close']    # close price
 
-        if next_color == 'Red':
+            if next_color == 'Red':
 
-            if current_color is 'Green':
-                sell_price = adj_close_price
-                funds = shares * sell_price
-                shares = 0.00
+                # want to sell all stocks if current week is Green
+                if current_color is 'Green':
+                    sell_price = adj_close_price
+                    funds = shares * sell_price
+                    shares = 0.00
 
-        elif next_color == 'Green':
+            elif next_color == 'Green':
 
-            if current_color is 'Red':
-                buy_price = open_price
-                shares = funds / buy_price
-                funds = 0.00
+                # want to buy max amt of stocks if current week is Red
+                if current_color is 'Red':
+                    buy_price = open_price
+                    shares = funds / buy_price
+                    funds = 0.00
 
-        """
-        print('\n ---------- Iteration', i, '----------')
-        print("Current Color: ", current_color)
-        print("Next Color: ", next_color)
-        print("Open Price: ", open_price)
-        print("Close Price: ", adj_close_price)
-        print("Funds After: ", funds)
-        print("Shares After: ", shares)
-        """
+            current_color = next_color  # set color to next week's color
 
-        current_color = next_color
+        # If shares haven't been converted to funds by the end of time period
+        if shares > 0:
 
-    if shares > 0:
-        final_value = week_data.iloc[len(week_data)-1]['Adj Close'] * shares
+            # convert shares to funds at final adjusted closing price
+            final_value = week_data.iloc[len(week_data)-1]['Adj Close'] * shares
 
-        print("Final value: ", final_value)
+        # all shares already sold
+        else:
+            final_value = funds
 
-    else:
-        final_value = funds
+        return final_value
 
-    return final_value
+    except KeyError as color_strat_error:
+        print(color_strat_error)
+        print('Key not found while performing color strategy')
 
 
-print("Buy & Hold for 2015: ", buy_and_hold(df[df['Year'] == 2015]).round(2))
-print("Color Strategy for 2015: ", color_strategy(df[df['Year'] == 2015]).round(2))
+if __name__ == "__main__":
+
+    print("\nQuestion 1")
+    # Buy and hold strategy applied to 2015
+    buy_and_hold_final = buy_and_hold(df[df['Year'] == 2015]).round(2)
+    print("Buy & Hold for 2015: " + "$" + str(buy_and_hold_final))
+
+    print("\nQuestion 2")
+    # Color strategy applied to 2015
+    color_strat_final = color_strategy(df[df['Year'] == 2015]).round(2)
+    print("Color Strategy for 2015: " + "$" + str(color_strat_final))
 
