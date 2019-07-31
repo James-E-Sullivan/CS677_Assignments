bcba11a2bf6ef8953675bac2a35add04790b4d84
James Sullivan <jesullivan222@gmail.com>
2019-07-31 09:00:24 -0400

    Cleaned up code and added comments for logistic regression and knn. Still need to add more comments and write-up results for all.

--- Assignment3/kNN.py
+++ Assignment3/kNN.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import LabelEncoder
 from Assignment2 import week_labeling as wl
 from Assignment3 import mu_sigma_feature_set as fs
 
@@ -10,133 +9,153 @@ pd.set_option('display.max_rows', 100)
 pd.set_option('display.width', 500)
 pd.set_option('display.max_columns', 50)
 
+# obtain DataFrame of ticker file
 data = fs.get_ticker_df()
 
+# split data into DataFrames for 2017 and 2018
 df_2017 = data[data['Year'] == 2017].reset_index()
 df_2018 = data[data['Year'] == 2018].reset_index()
 
-input_2017 = df_2017[['Year_Week', 'Mean_Return', 'Std_Return', 'Color']].groupby('Year_Week').max().reset_index()
-input_2017['Class'] = input_2017['Color'].apply(lambda x: 1 if x is 'Green' else 0)
+# obtain smaller df of 2017 data, grouped by week
+input_2017 = df_2017[['Year_Week', 'Mean_Return', 'Std_Return', 'Color',
+                      'Class']].groupby('Year_Week').max().reset_index()
 
+# obtain X and Y values for kNN classification
 X = input_2017[['Mean_Return', 'Std_Return']].values
 Y = input_2017['Class'].values
 
+k_values = [3, 5, 7, 9, 11]  # list of k values to test
 
-#print(X)
-#print(Y)
+# obtain smaller df of 2018 data, grouped by week
+output_2018 = df_2018[['Year_Week', 'Mean_Return', 'Std_Return', 'Color',
+                       'Class']].groupby('Year_Week').max().reset_index()
 
-k_values = [3, 5, 7, 9, 11]
-
-output_2018 = df_2018[['Year_Week', 'Mean_Return', 'Std_Return', 'Color']].groupby('Year_Week').max().reset_index()
-output_2018['Class'] = output_2018['Color'].apply(lambda x: 1 if x is 'Green' else 0)
-
-
+# initialize accuracy_list for accuracy values
 accuracy_list = []
 
-
-
-
-
+print('k value accuracy for 2017: ')
 
 # obtain accuracy for each k in k_values
 for k in k_values:
 
+    # get kNN classifier for k, fit to X and Y from above
     knn_classifier = KNeighborsClassifier(n_neighbors=k)
     knn_classifier.fit(X, Y)
-    #new_instance = output_2018[['Mean_Return', 'Std_Return']].values
 
+    # predict 2017 labels with X and kNN_classifier (to find best k value)
     new_instance = input_2017[['Mean_Return', 'Std_Return']].values
-
     prediction = knn_classifier.predict(new_instance)
+
+    # add predictions for each k value as new column to 2017 df
     column_name = 'k_' + str(k)
-
-    #actual_values = output_2018['Class'].values
-    actual_values = input_2017['Class'].values
-
-    #output_2018[column_name] = prediction
     input_2017[column_name] = prediction
 
+    # create accuracy column in df (1 for correct prediction, else 0)
     accuracy_name = column_name + '_acc'
+    input_2017[accuracy_name] = input_2017[['Class', column_name]].apply(
+        fs.get_acc, axis=1)
 
-    #output_2018[accuracy_name] = output_2018[['Class', column_name]].apply(fs.get_acc, axis=1)
-    input_2017[accuracy_name] = input_2017[['Class', column_name]].apply(fs.get_acc, axis=1)
-
-    #correct_count = output_2018[accuracy_name].sum()
-    #total_weeks = output_2018[accuracy_name].count()
-
+    # calculate accuracy for each k value (ratio not percentage)
     correct_count = input_2017[accuracy_name].sum()
     total_weeks = input_2017[accuracy_name].count()
-
     accuracy = correct_count / total_weeks
 
+    # append accuracy to accuracy_list
     accuracy_list.append(accuracy)
 
-    print(accuracy_name + ':', accuracy.round(2))
+    # output accuracy and k values to console
+    print(accuracy_name + ':', round(accuracy, 4))
 
 
 def kNN_predict(df1, k1, X1, Y1):
+    """
+    Add predicted class and color values to a DataFrame using
+    kNN classifier.
+    :param df1: input DataFrame
+    :param k1: k value for kNN
+    :param X1: X value from training set
+    :param Y1: Y value from training set
+    :return df1: df w/ appended predicted class & color
+    """
 
-    knn_classifier = KNeighborsClassifier(n_neighbors=k1)
-    knn_classifier.fit(X1, Y1)
+    # get kNN classifier for k, fit to X and Y
+    knn_classifier1 = KNeighborsClassifier(n_neighbors=k1)
+    knn_classifier1.fit(X1, Y1)
 
-    new_instance = df1[['Mean_Return', 'Std_Return']].values
+    # predict label based on df1 feature-set
+    new_df_features = df1[['Mean_Return', 'Std_Return']].values
+    new_df_prediction = knn_classifier1.predict(new_df_features)
 
-    prediction = knn_classifier.predict(new_instance)
-
-    df1['Pred_Class'] = prediction
-    df1['Pred_Color'] = df1['Pred_Class'].apply(lambda x: 'Green' if x is 1 else 'Red')
+    # add predicted class and labels to df1
+    df1['Pred_Class'] = new_df_prediction
+    df1['Pred_Color'] = df1['Pred_Class'].apply(
+        lambda x: 'Green' if x is 1 else 'Red')
 
     return df1
 
-#k_acc_df = pd.DataFrame(accuracy_list, columns=['k', 'accuracy'])
+
+# create df for 2017 k and accuracy values
 k_acc_df = pd.DataFrame()
 k_acc_df['k'] = k_values
 k_acc_df['accuracy'] = accuracy_list
 
+# get max k value(s) from k_acc_df
 k_acc_max = k_acc_df.loc[k_acc_df['accuracy'] == k_acc_df['accuracy'].max()]
 
 
-print(k_acc_df)
-print(k_acc_max)
+print('\n__________Question 1__________')
 
+# optimal_k is set to first k value with maximum accuracy
 optimal_k = int(k_acc_max.iloc[0][0])
-
 print('Optimal k value for year 1: ', optimal_k)
 
 
+print('\n__________Question 2__________')
+
 # predict labels for year 2
-
-
 output_2018.update(kNN_predict(output_2018, optimal_k, X, Y))
-output_2018['Accuracy'] = output_2018[['Color', 'Pred_Color']].apply(fs.get_acc, axis=1)
+output_2018['Accuracy'] = output_2018[['Color', 'Pred_Color']].apply(
+    fs.get_acc, axis=1)
 
 accuracy_2018 = output_2018['Accuracy'].sum() / output_2018['Accuracy'].count()
-print('Accuracy for 2018: ', accuracy_2018.round(2))
+print('Accuracy for 2018: ', round(accuracy_2018, 4))
 
+
+print('\n__________Question 3__________')
+
+# calculate true/false positive/negative values for 2018 predictions
 tp_sum = output_2018[['Color', 'Pred_Color']].apply(fs.tp, axis=1).sum()
 fp_sum = output_2018[['Color', 'Pred_Color']].apply(fs.fp, axis=1).sum()
 tn_sum = output_2018[['Color', 'Pred_Color']].apply(fs.tn, axis=1).sum()
 fn_sum = output_2018[['Color', 'Pred_Color']].apply(fs.fn, axis=1).sum()
 
+# calculate confusion matrix
 confusion_2018 = np.array([[tn_sum, fp_sum], [fn_sum, tp_sum]])
-
 print('\nConfusion Matrix for 2018: \n', confusion_2018)
 
+
+print('\n__________Question 4__________')
+
+# calculate true positive rate and true negative rate
 tpr = tp_sum / (tp_sum + fn_sum)
 tnr = tn_sum / (tn_sum + fp_sum)
 
-print('\nTrue Positive Rate: ', tpr.round(2))
-print('\nTrue Negative Rate: ', tnr.round(2))
+# print tpr and tnr values
+print('\nTrue Positive Rate: ', round(tpr, 4))
+print('True Negative Rate: ', round(tnr, 4))
 
+
+print('\n__________Question 5__________')
+
+# set df_2018 color value to predicted color so color_strat_knn uses pred values
 df_2018['Color'] = output_2018['Pred_Color']
 
-color_strat_knn = wl.color_strategy(df_2018).round(2)
+color_strat_knn = round(wl.color_strategy(df_2018), 2)
 print('Color Strategy for 2018: ' + '$' + str(color_strat_knn))
 
-buy_and_hold_knn = wl.buy_and_hold(df_2018).round(2)
+buy_and_hold_knn = round(wl.buy_and_hold(df_2018), 2)
 print('Buy & Hold for 2018: ' + '$' + str(buy_and_hold_knn))
 
-#output_2018['']
-
-#print(output_2018)
+print('\nBuy and Hold Strategy results in a larger'
+      ' amount at the end of the year.')
 
--- Assignment3/logistic_regression.py
+++ Assignment3/logistic_regression.py
@@ -5,18 +5,24 @@ import os
 from Assignment2 import week_labeling as wl
 from Assignment3 import mu_sigma_feature_set as fs
 
+# allow df prints to display each column
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.width', 500)
+pd.set_option('display.max_columns', 50)
+
+# obtain DataFrame of ticker file
 data = fs.get_ticker_df()
 
+# split data into DataFrames for 2017 and 2018
 df_2017 = data[data['Year'] == 2017].reset_index()
 df_2018 = data[data['Year'] == 2018].reset_index()
 
+# obtain smaller df of 2017 data as a training set
 training_set = df_2017[[
     'Year_Week', 'Mean_Return', 'Std_Return', 'Color', 'Class']].groupby(
     'Year_Week').max().reset_index()
 
-#print(training_set.head())
-
-
+# obtain x and y values for logitic regression
 x = training_set[['Mean_Return', 'Std_Return']].values
 y = training_set['Class'].values
 
@@ -25,6 +31,11 @@ learning_rate = 0.01
 
 
 def sigmoid(z):
+    """
+    sigmoid function for a given value z
+    :param z: input value
+    :return: output of sigmoid function
+    """
     return 1.0 / (1.0 + np.exp(-z))
 
 
@@ -33,6 +44,7 @@ def loss(h, y):
 
 
 def add_intercept(x):
+
     intercept = np.ones((x.shape[0], 1))
     return np.concatenate((intercept, x), axis=1)
 
@@ -45,7 +57,17 @@ x = add_intercept(x)
 weights = np.zeros(x.shape[1])
 
 
-def compute_weights(x,weights,iterations, learning_rate, debug_step=1000):
+def compute_weights(x, weights, iterations, learning_rate, debug_step=1000):
+    """
+    Compute weights for regression
+    :param x: feature-set
+    :param weights: blank np array
+    :param iterations: number of iterations to be used
+    :param learning_rate: learning rate of logistic regression
+    :param debug_step: Step for debugging if output message is to be
+    displayed
+    :return:
+    """
     for i in range(iterations):
         y_pred = np.dot(x, weights)
         phi = sigmoid(y_pred)
@@ -54,128 +76,95 @@ def compute_weights(x,weights,iterations, learning_rate, debug_step=1000):
         if i % debug_step==0:
             y_pred = np.dot(x, weights)
             phi = sigmoid(y_pred)
-            #print('i:', i, 'loss: ', loss(phi, y_pred))
-    #print('rate: ', learning_rate, ' iterations ', iterations, ' weights: ', weights)
     return weights
 
-# compute weights
+
+print('\n__________Question 1__________')
+
+# weights np array filled with computed weights
 weights = compute_weights(x,weights,iterations=iterations, learning_rate=learning_rate,
                           debug_step=1000)
 
-print(weights[0])
+# output equation
+print('Equation for logistic regression from 2017: ')
+print('y = ' + str(round(weights[0], 4)) + ' + (' + str(round(weights[1], 4))
+      + ' * mu) + (' + str(round(weights[2], 4)) + ' * sigma)')
+print('z(y) = 1 / (1 + e^(-y))')
 
 
 def predict_y(mu, sigma, weight_list):
     """
     Apply to mu and sigma values in df
-    :param feature_values: (mu, sigma)
+    :param mu: mean
+    :param sigma: standard deviation
     :param weight_list: Weights
     :return: Predicted y value for lin reg
     """
     return weight_list[0] + (weight_list[1] * mu) + \
         (weight_list[2] * sigma)
 
-"""
-def get_acc(vector):
 
-    if vector[0] is vector[1]:
-        return 1
-    else:
-        return 0
+print('\n__________Question 2__________')
 
+# predict color labels for 2018
+df_2018['Linear_Y'] = df_2018.apply(
+    lambda b: predict_y(b['Mean_Return'], b['Std_Return'], weights), axis=1)
 
-def tp(vector):
-
-    if vector[0] is 'Green' and vector[1] is 'Green':
-        return 1
-    else:
-        return 0
-
-
-def fp(vector):
-
-    if vector[0] is 'Red' and vector[1] is 'Green':
-        return 1
-    else:
-        return 0
-
-
-def tn(vector):
-
-    if vector[0] is 'Red' and vector[1] is 'Red':
-        return 1
-    else:
-        return 0
-
-
-def fn(vector):
-
-    if vector[0] is 'Green' and vector[1] is 'Red':
-        return 1
-    else:
-        return 0
-"""
-
-#df_2018['Feature_Values'] = df_2018[['Mean_Return', 'Std_Return']].apply(lambda b: (b[0], b[1]))
-
-
-df_2018['Linear_Y'] = df_2018.apply(lambda b: predict_y(b['Mean_Return'], b['Std_Return'], weights), axis=1)
 df_2018['Logistic_Y'] = df_2018[['Linear_Y']].apply(sigmoid, axis=1)
-df_2018['Predicted_Color'] = df_2018['Logistic_Y'].apply(lambda c: 'Green' if c > 0.5 else 'Red')
 
-
+df_2018['Predicted_Color'] = df_2018['Logistic_Y'].apply(
+    lambda c: 'Green' if c > 0.5 else 'Red')
 
 
 # Smaller df with predicted 2018 values, grouped by 'Year_Week'
-pred_2018 = df_2018[['Year_Week', 'Logistic_Y', 'Color', 'Predicted_Color']].groupby(['Year_Week']).max()
+pred_2018 = df_2018[
+    ['Year_Week', 'Logistic_Y', 'Color', 'Predicted_Color']].groupby(
+    ['Year_Week']).max()
 
-pred_2018['Accuracy'] = pred_2018[['Color', 'Predicted_Color']].apply(fs.get_acc, axis=1)
+pred_2018['Accuracy'] = pred_2018[['Color', 'Predicted_Color']].apply(
+    fs.get_acc, axis=1)
 
-#print(predicted_2018)
 
+# calculate accuracy of predicted values for 2018
 correct_count = pred_2018['Accuracy'].sum()
 total_weeks = pred_2018['Accuracy'].count()
-
 accuracy = correct_count / total_weeks
+
 percent_accuracy = accuracy * 100
-print(accuracy.round(2))
+print('Percent Accuracy for 2018 predicted values', round(percent_accuracy, 2))
 
-"""
-pred_2018['TP'] = pred_2018[['Color', 'Predicted_Color']].apply(tp, axis=1)
-pred_2018['FP'] = pred_2018[['Color', 'Predicted_Color']].apply(fp, axis=1)
-pred_2018['TN'] = pred_2018[['Color', 'Predicted_Color']].apply(tn, axis=1)
-pred_2018['FN'] = pred_2018[['Color', 'Predicted_Color']].apply(fn, axis=1)
-"""
 
+print('\n__________Question 3__________')
+
+# calculate true/false positive/negative values for 2018 predictions
 tp_sum = pred_2018[['Color', 'Predicted_Color']].apply(fs.tp, axis=1).sum()
 fp_sum = pred_2018[['Color', 'Predicted_Color']].apply(fs.fp, axis=1).sum()
 tn_sum = pred_2018[['Color', 'Predicted_Color']].apply(fs.tn, axis=1).sum()
 fn_sum = pred_2018[['Color', 'Predicted_Color']].apply(fs.fn, axis=1).sum()
 
-print(pred_2018)
-
-""""
-confusion_2018 = np.array([[pred_2018['TN'].sum(), pred_2018['FP'].sum()],
-                           [pred_2018['FN'].sum(), pred_2018['TP'].sum()]])
-"""
-
+# calculate and output confusion matrix
 confusion_2018 = np.array([[tn_sum, fp_sum], [fn_sum, tp_sum]])
-
-tpr = tp_sum / (tp_sum + fn_sum)
-tnr = tn_sum / (tn_sum + fp_sum)
-#test_array = np.array([0, 1], [])
-
 print("\nConfusion Matrix: \n", confusion_2018)
 
-print("\nTrue Positive Rate: ", tpr)
-print("\nTrue Negative Rate: ", tnr)
+print('\n__________Question 4__________')
 
+# calculate true positive rate and true negative rate
+tpr = tp_sum / (tp_sum + fn_sum)
+tnr = tn_sum / (tn_sum + fp_sum)
+
+# print tpr and tnr values
+print("\nTrue Positive Rate: ", tpr)
+print("True Negative Rate: ", tnr)
+
+print('\n___________Question 5___________')
+
+# set df_2018 color value to predicted color so color_strat_log uses pred values
 df_2018['Color'] = df_2018['Predicted_Color']
 
-color_strat_final = wl.color_strategy(df_2018).round(2)
-print("Color Strategy for 2018: " + "$" + str(color_strat_final))
+color_strat_log = round(wl.color_strategy(df_2018), 2)
+print("Color Strategy for 2018: " + "$" + str(color_strat_log))
 
-buy_and_hold_final = wl.buy_and_hold(df_2018).round(2)
+buy_and_hold_final = round(wl.buy_and_hold(df_2018), 2)
 print("Buy & Hold for 2018: " + "$" + str(buy_and_hold_final))
 
 
